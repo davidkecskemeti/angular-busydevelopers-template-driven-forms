370a22fb301befb8927ced41bd067b917fda84ed
Dávid Kecskeméti <dxdy421@gmail.com>
2019-10-09 16:35:24 +0200

    kd

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgModel} from '@angular/forms';
 
 @Component({
-  selector: 'app-contact-form',
+  // tslint:disable-next-line:component-selector
+  selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  log(firstName) {
+    console.log(firstName);
+  }
+
+  submit(f) {
+    console.log(f);
+  }
 }
